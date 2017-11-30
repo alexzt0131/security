8d321c6771a4f0b820eef47927854092d0b0db17
alexzt0131 <110021341@qq.com>
2017-11-30 10:03:14 +0800

    configure uwsgi step 1

--- security/tools/run.py
+++ security/tools/run.py
@@ -7,18 +7,17 @@ django.setup()
 
 
 
-from security_tj.settings import STATICFILES_DIRS
+from security_tj.settings import STATICFILES_DIRS, BASE_DIR
 
 if __name__ == '__main__':
     pass
 
-
-    with open(STATICFILES_DIRS[0] + '/docs/zhaopin.txt') as f:
-        lines = f.readlines()
-
-
-    for i in lines:
-        print(i)
+    # with open(STATICFILES_DIRS[0] + '/docs/zhaopin.txt') as f:
+    #     lines = f.readlines()
+    #
+    #
+    # for i in lines:
+    #     print(i)
     # rootdir = STATICFILES_DIRS[0] + '/images/xuanchuan/'  # 指明被遍历的文件夹
     # print(rootdir)
     #
